93e9d4ce74cb1c0b2cef7eef43d2ea981d928772
djkepa <deejaykepa@gmail.com>
2020-08-11 23:17:11 +0200

    Vogue app with context api and hooks

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+	const priceForStripe = price * 100;
+	const publishableKey =
+		'pk_test_51H9u1kLmDvIoUOuhl1PpmK1n4or5g1Ul8Ifd62XXnqKWfgpk9nBwCxx4w59GVAbiQi4Z5vCwj8TUXjep6esEcaWr0098To7Bg1';
+
+	const onToken = (token) => {
+		console.log(token);
+		alert('Payment Succesful!');
+	};
+
+	return (
+		<StripeCheckout
+			label="Pay Now"
+			name="Vogue Ltd."
+			billingAddress
+			shippingAddress
+			image=""
+			description={`Your total is $${price}`}
+			amount={priceForStripe}
+			panelLabel="Pay Now"
+			token={onToken}
+			stripeKey={publishableKey}
+		/>
+	);
+};
+
+export default StripeCheckoutButton;
---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,50 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+  apiKey: "AIzaSyD0_OelJpLEW6J0nFC3n81hSXroY93iUc4",
+  authDomain: "vogue-app.firebaseapp.com",
+  databaseURL: "https://vogue-app.firebaseio.com",
+  projectId: "vogue-app",
+  storageBucket: "vogue-app.appspot.com",
+  messagingSenderId: "915584661211",
+  appId: "1:915584661211:web:a8c409a77c7dd40ac8fefe",
+  measurementId: "G-5LZZ3KW0Y2"
+};
+
+firebase.initializeApp(config);
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (error) {
+      console.log('error creating user', error.message);
+    }
+  }
+
+  return userRef;
+};
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
